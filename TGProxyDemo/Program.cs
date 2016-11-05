3d9fe2ca017da34d504ab4f0336260afb3b8ddf7
alisinabh <alisina.bm@gmail.com>
2016-11-05 20:27:11 +0330

    abstractly implemnted. features and commands left

---
+++ TGProxyDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using TGProxyDemo.Users;
+using TweetSharp;
+
+namespace TGProxyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var twBot = new TweetBot.TweetBot("213125170:AAGTgyQ3NLOs_2O6nacchnEVkDTaXinO65E", "ZlutArK4D8yisGNV80MX2UdtW", "4KoD3fGKqLkDRwdYIJ3mJGgG2Lz6GoPIK93zgWnrCnKziy7XNO");
+
+            twBot.LoadBotUsers();
+            twBot.StartBot();
+
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+ 
+    }
+}
---
+++ TGProxyDemo/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using TweetSharp;
+
+namespace TGProxyDemo.Users
+{
+    public class User
+    {
+        public long ChatId { get; set; }
+
+        [JsonIgnore]
+        public TwitterService TwService { get; set; }
+
+        public OAuthAccessToken AccessToken { get; set; }
+
+        public OAuthRequestToken RequestToken { get; set; }
+
+        public bool IsVerified { get; set; }
+    }
+}
