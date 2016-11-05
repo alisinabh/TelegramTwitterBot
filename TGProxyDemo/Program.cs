aa680fd71e1e936a5889d48b60fdc03a96bc1cfa
alisinabh <alisina.bm@gmail.com>
2016-11-06 00:49:01 +0330

    Basic twitter ops working like a charm :D

--- TGProxyDemo/Program.cs
+++ TGProxyDemo/Program.cs
@@ -10,7 +10,7 @@ namespace TGProxyDemo
     {
         static void Main(string[] args)
         {
-            var twBot = new TweetBot.TweetBot("213125170:AAGTgyQ3NLOs_2O6nacchnEVkDTaXinO65E", "ZlutArK4D8yisGNV80MX2UdtW", "4KoD3fGKqLkDRwdYIJ3mJGgG2Lz6GoPIK93zgWnrCnKziy7XNO");
+            var twBot = new TweetBot.TweetBot("213125170:AAGTgyQ3NLOs_2O6nacchnEVkDTaXinO65E", "DSAp0Zy4p8J8fbAzcyuFndhR2", "mcK4QMKRp4IRJghvf6utv7eOO9ER8emTI9EGayqFH5XgMyFedA");
 
             twBot.LoadBotUsers();
             twBot.StartBot();
